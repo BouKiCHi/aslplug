ed62fa3041312efb87c156ae24c9f20e2a32bdbf
bkc <alf_bincyou@hotmail.com>
2014-11-06 13:08:36 +0900

    first commit

---
+++ src/nestypes.h
@@ -0,0 +1,63 @@
+#ifndef NESTYPES_H__
+#define NESTYPES_H__
+
+#if defined(_MSC_VER)
+#define NEVER_REACH __assume(0);
+#elif defined(__BORLANDC__)
+#define __fastcall __msfastcall
+#elif defined(__GNUC__)
+#define __inline		__inline__
+#ifndef __fastcall
+#define __fastcall
+#endif
+#else
+#define __inline
+#define __fastcall
+#endif
+#ifndef NEVER_REACH
+#define NEVER_REACH
+#endif
+
+typedef int				Int;
+typedef unsigned int	Uint;
+typedef signed int		Int32;
+typedef signed long		Int64;
+#ifndef Uint32
+#define Uint32 unsigned int
+#endif
+#ifndef Uint8
+#define Uint8 unsigned char
+#endif
+typedef signed short	Int16;
+typedef signed char		Int8;
+typedef char			Char;
+
+#ifndef USE_SDL
+typedef unsigned long	Uint64;
+typedef unsigned short	Uint16;
+#endif
+
+#ifdef WIN32
+#include <malloc.h>
+#include <memory.h>
+#else
+#include <stdlib.h>
+#include <string.h>
+#endif
+
+
+#ifndef WIN32
+#include <stdlib.h>
+#define XSLEEP(t)		_sleep(t)
+#else
+#include <windows.h>
+#include <stdlib.h>
+#define XSLEEP(t)		sleep(t)
+#endif
+#define XMALLOC(s)		malloc(s)
+#define XREALLOC(p,s)	realloc(p,s)
+#define XFREE(p)		free(p)
+#define XMEMCPY(d,s,n)	memcpy(d,s,n)
+#define XMEMSET(d,c,n)	memset(d,c,n)
+
+#endif /* NESTYPES_H__ */
